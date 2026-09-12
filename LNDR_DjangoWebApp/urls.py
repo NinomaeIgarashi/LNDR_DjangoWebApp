f5f8fc8197917281c72f0ba2fa7660e78f425f30
Luis Nicholas Daculug Reyes <assbad931@gmail.com>
2026-09-13 01:49:41 +0800

    Add REST API authentication and session management

--- LNDR_DjangoWebApp/urls.py
+++ LNDR_DjangoWebApp/urls.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("home.urls")),
-    path('registration/',include('registration.urls'))
+    path('admin/', admin.site.urls),
+
+    path(
+        'registration/',
+        include('registration.urls')
+    ),
+
+    path(
+        'api/',
+        include('registration.api_urls')
+    ),
+
+    path(
+        'accounts/',
+        include('django.contrib.auth.urls')
+    ),
 ]
