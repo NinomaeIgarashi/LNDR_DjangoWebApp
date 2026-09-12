e3be7d720663543f9faf5f4dcbb0dacd91e7fd2f
Luis Nicholas Daculug Reyes <assbad931@gmail.com>
2026-09-13 01:05:54 +0800

    Add student dashboard for Module 04

--- registration/urls.py
+++ registration/urls.py
@@ -4,27 +4,14 @@ from . import views
 
 urlpatterns = [
 
-    path(
-        '',
-        views.student_list,
-        name='student_list'
-    ),
+    path('', views.student_list, name='student_list'),
+    path('add/', views.student_create, name='student_create'),
+    path('edit/<int:pk>/', views.student_update, name='student_update'),
+    path('delete/<int:pk>/', views.student_delete, name='student_delete'),
 
     path(
-        'add/',
-        views.student_create,
-        name='student_create'
-    ),
-
-    path(
-        'edit/<int:pk>/',
-        views.student_update,
-        name='student_update'
-    ),
-
-    path(
-        'delete/<int:pk>/',
-        views.student_delete,
-        name='student_delete'
-    ),
+        'dashboard/',
+        views.student_dashboard,
+        name='student_dashboard'
+    )
 ]
